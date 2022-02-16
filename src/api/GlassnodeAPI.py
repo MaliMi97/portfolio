0b957ecac1c8d6bbd43b6d9e41800f8e79999326
Michal Malik <malikmi3@fjfi.cvut.cz>
2022-02-16 19:13:28 +0100

    new addresses

--- src/api/GlassnodeAPI.py
+++ src/api/GlassnodeAPI.py
@@ -1,5 +1,6 @@
 import datetime
 import pandas as pd
+import numpy as np
 from . import API
 
 class GlassnodeAPI(API.API):
@@ -12,7 +13,7 @@ class GlassnodeAPI(API.API):
         super().__init__(time_out, sleep, good_result)
         self.key = key
 
-    def get_dataframe(self, response, column_names, period):
+    def get_dataframe_no_accumulation(self, response, column_names, period):
         '''
         Makes a data frame from a response and changes it, so that it contains either daily or weekly data. Also changes the names of columns.
         '''
@@ -29,11 +30,31 @@ class GlassnodeAPI(API.API):
             raise Exception("The only viable periods are day and week")
         return df
 
+    def get_dataframe_accumulation(self, response, column_names, period):
+        '''
+        Makes a data frame from a response and changes it, so that it contains either daily or weekly data. Also changes the names of columns.
+        '''
+        if period == 'day':
+            return self.get_dataframe_no_accumulation(response, column_names, period)
+        elif period == 'week':
+            df = pd.DataFrame(response)
+            df.columns = column_names
+            df['time'] = df['time'].apply(datetime.datetime.fromtimestamp)
+            df['aux'] = [np.sum(df[column_names[-1]][:i+1]) for i in np.arange(6)] + [np.sum(df[column_names[-1]][i-6:i+1]) for i in np.arange(6,len(df))]
+            df = df.drop(columns=[column_names[-1]])
+            df.columns = column_names
+            df = df[df['time'].apply(datetime.date.weekday) == 6]
+            df = df.reset_index()
+            df = df.drop(['index'],axis=1)
+            return df
+        else:
+            raise Exception("The only viable periods are day and week")
+
     def get_closing_price(self, coin='btc', period='day'):  
         '''
         Returns dataframe containing either daily (period='day') or weekly (period='week') closing price of a coin (default is bitcoin).
         ''' 
-        return self.get_dataframe(\
+        return self.get_dataframe_no_accumulation(\
                 self.get_response(self.base_api_path+'v1/metrics/market/price_usd_close', {'a': coin, 'api_key': self.key}),\
                 ['time', 'price'], period)
 
@@ -41,7 +62,7 @@ class GlassnodeAPI(API.API):
         '''
         Returns dataframe containing either daily (period='day') or weekly (period='week') market cap of a coin (default is bitcoin).
         ''' 
-        return self.get_dataframe(\
+        return self.get_dataframe_no_accumulation(\
                 self.get_response(self.base_api_path+'v1/metrics/market/marketcap_usd', {'a': coin, 'api_key': self.key}),\
                 ['time', 'market cap'], period)
 
@@ -49,7 +70,7 @@ class GlassnodeAPI(API.API):
         '''
         Returns dataframe containing either daily (period='day') or weekly (period='week') realized price of a coin (default is bitcoin).
         ''' 
-        return self.get_dataframe(\
+        return self.get_dataframe_no_accumulation(\
                 self.get_response(self.base_api_path+'v1/metrics/market/price_realized_usd', {'a': coin, 'api_key': self.key}),\
                 ['time', 'realized price'], period)
 
@@ -57,7 +78,7 @@ class GlassnodeAPI(API.API):
         '''
         Returns dataframe containing either daily (period='day') or weekly (period='week') realized cap of coin (default is bitcoin).
         ''' 
-        return self.get_dataframe(\
+        return self.get_dataframe_no_accumulation(\
                 self.get_response(self.base_api_path+'v1/metrics/market/marketcap_realized_usd', {'a': coin, 'api_key': self.key}),\
                 ['time', 'realized cap'], period)
 
@@ -67,6 +88,18 @@ class GlassnodeAPI(API.API):
         ''' 
         return self.get_closing_price(coin, period).merge(self.get_realized_price(coin, period), on='time')
     
+    def get_new_addresses(self, coin='btc', period='day'):  
+        '''
+        Returns dataframe containing either daily (period='day') or weekly (period='week') new addresses holding coin (default is bitcoin).
+        ''' 
+        return self.get_dataframe_accumulation(\
+                self.get_response(self.base_api_path+"v1/metrics/addresses/new_non_zero_count", {'a': coin, 'api_key': self.key}),\
+                ['time', 'number of new addresses'], period)
 
+    def get_new_addresses_and_price(self, coin='btc', period='day'):  
+        '''
+        Returns dataframe containing either daily (period='day') or weekly (period='week') new addresses holding coin (default is bitcoin) and the price of coin at that time.
+        ''' 
+        return self.get_closing_price(coin, period).merge(self.get_new_addresses(coin, period), on='time')
     
 
